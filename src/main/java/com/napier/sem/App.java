4f1789a6865bdcbfb2ec1a450b4659d1141e61a3
calan <kenmurecalan@gmail.com>
2025-10-04 10:34:41 +0100

    updated template Java cause wasnt printing

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,4 +1,6 @@
 package com.napier.sem;
+
+
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
@@ -6,21 +8,16 @@ import org.bson.Document;
 
 public class App
 {
-
     public static void main(String[] args)
     {
         // Connect to MongoDB
-
         MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Get a database - will create when we use it
-
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
-
         MongoCollection<Document> collection = database.getCollection("test");
         // Create a document to store
-
-        Document doc = new Document("name", "Test Text")
+        Document doc = new Document("name", "Kevin Sim")
                 .append("class", "DevOps")
                 .append("year", "2024")
                 .append("result", new Document("CW", 95).append("EX", 85));
